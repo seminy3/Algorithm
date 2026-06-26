bbe0018d05a0c033ff2520aca89f1e82582d89b2
seminy._. <seminy333@gmail.com>
2026-06-26 16:25:56 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 101.41 ms, Memory: 26.2 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.py
@@ -1,16 +1,8 @@
 def solution(arr):
     x = 1
-    i = 1
     list_1 = [arr[0]]
-    while True:
-        if (x >= len(arr)):
-            break
-        elif (list_1[i-1] != arr[x]):
+    while x < len(arr):              
+        if list_1[-1] != arr[x]:     
             list_1.append(arr[x])
-            x += 1
-            i += 1
-        elif (list_1[i-1] == arr[x]):
-            x += 1
-            
-    answer = list_1
-    return answer
+        x += 1                       
+    return list_1
